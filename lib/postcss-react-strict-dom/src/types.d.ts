e339b677db2d1e49da2495db23294bf5f63df523
Tsubasa Sakai <javascripter@tsukkun.net>
2024-11-10 12:17:35 +0000

    use the published version of postcss-react-strict-dom

--- lib/postcss-react-strict-dom/src/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-strict-dom/babel-preset' {
-  const reactStrictBabelPreset: {
-    generateStyles(rules: string[]): string
-  }
-
-  export default reactStrictBabelPreset
-}
--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,17 @@ module.exports = {
         'src/**/*.{js,jsx,ts,tsx}',
         'node_modules/react-strict-dom/dist/**/*.{js,jsx,ts,tsx}',
       ],
+      babelConfig: {
+        presets: [
+          [
+            'next/dist/compiled/babel/preset-typescript',
+            {
+              allowNamespaces: true,
+            },
+          ],
+          ['react-strict-dom/babel-preset', { rootDir: process.cwd() }],
+        ],
+      },
     },
     autoprefixer: {},
   },
